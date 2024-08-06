882d702612315c61268637fa5422622849dafdbb
carol <carol@LAPTOP-IJ353F7M>
2024-08-06 16:49:42 +0400

    Création du fichier models

---
+++ lib/models/ice_cream_shop.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+class IceCreamShop {
+  // ice cream for sale list
+
+  // user cart
+
+  // get ice cream list
+
+  //get user cart
+
+  // add ice cream to cart
+
+  // remove ice cream from cart
+}
